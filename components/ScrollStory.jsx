83115f0bec78fc23b866209b6b004087ca90dccd
emaanfatima312005-wq <emaan.fatima.312005@gmail.com>
2026-09-05 23:57:09 +0500

    Building home page

---
+++ components/ScrollStory.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function ScrollStory() {
+  return (
+    <section
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#fffdf8",
+      }}
+    >
+      <h1 style={{ fontSize: "70px" }}>
+        ScrollStory works ☀️
+      </h1>
+    </section>
+  );
+}
